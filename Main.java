88402ad11878f98d57a3435005eb4235fa84bf8b
Xx-Aiser-xX <stas.plot@mail.ru>
2024-11-22 02:06:47 +0300

    add Main

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+
+
+    }
+
+    public int sum(int a, int b){
+        return a + b;
     }
 } 
